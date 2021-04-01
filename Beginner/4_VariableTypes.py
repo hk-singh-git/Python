ca8ec9382bff944bfed732ce13ae1990424c917a
Harry <hk@hk-singh.com>
2021-04-01 14:52:00 +0200

    Added Operators

--- Beginner/4_VariableTypes.py
+++ Beginner/4_VariableTypes.py
@@ -3,15 +3,32 @@
 
 # Dybamically typed - If the type is associated with run-time values and not with named variables. Values tored in variable hav type and not the name or the variable itself. 
 
-score = 10
+# Variable types
+# Numbers: Integers, Floating point and Complex numbers
+# Booleans: Loval values indicating False, True and None
+# Strings: Ordered sequence of characters
+# Lists: Ordered mutable sequence of objects 
+# Tuples: Orderd immutable sequence of objects
+# Sets: Mutable collections of unordered unique objects
+# FrozenSets: Immutable collections of unordered unique objects 
+# Dictionaries: Collections of unordered key: Value pairs
 
+score = 10
 type(score) # this will return <type 'int'>
 
 name = 'Harry'
-
 type(name) # this will return <type 'str'>
 
 id(score) # this returns the memory value of the variable
+print(hex(id(name))) # this returns HEX value for the memory location of the variable
 
-hex(id(name)) # this returns HEX value for the memory location of the variable
+years = [2001, 2002, 2003] # Data type is List
+
+Value = (1, 'John', 3) # This is a Tuple
+
+vowels = {'a','e','i','o','u'} #this is a set, containing unique data types
+
+person = {'name':'Harry', 'location':'Netherlands'} #This is a Dict (Dictionary)
+
+fs = frozenset ((1,2,'abc','xyz')) #this is a frozen set
 
---
+++ Beginner/5_MathOperator.py
@@ -0,0 +1,48 @@
+#Numbers and math operators in Python
+
+#Arithmetic Operators
+
+# + addition 
+# - Subtraction 
+# / division
+# // floor or integer division
+# * multiplication
+# ** exponentiation or raising ot a power 
+# % modulus (mod)
+
+price = 5 
+price + 6 # result will be 11
+price - 3 # result will be 2
+a = 3.4 
+b = 2.3 
+a + b # this will return 5.699999999999999
+a / b # this will return 1.4782608695652175
+a // b # this will return 1.0 
+
+
+
+###########################################
+
+a = 1
+a = a + 1
+
+a += 1 # add 1 to a and assign it to a 
+
+###########################################
+# Built-in functions for math operations
+
+# divmod()
+divmod(12,5) # returns (2,2) Division and reminder 
+# pow()
+pow(2,128)
+
+# round()
+round(1.123456 , 5 )
+
+# sum()
+numbers = [1, 5, 66, 100]
+sum (numbers)
+# max()
+max (numbers)
+# min()
+min (numbers)
