db2137559733cb2b07f13dc37aac0b193ae992df
Harry <hk@hk-singh.com>
2021-04-02 12:39:29 +0200

    Added one file for complete basic Python

--- Beginner/1_HelloWorld.py
+++
@@ -1,3 +0,0 @@
-## This is my first Python Program
-
-print ('Hello World')
--- Beginner/3_Constant.py
+++
@@ -1,7 +0,0 @@
-# How do we define constants in Python
-
-HOURS_IN_A_DAY = 24 # 24 hours in a day
-PI = 3.142 # Value of pi
-WATER_BOILING_POINT = 100 # Boiling point of water
-
-# Constants are defined in UPPERCASE in Python
